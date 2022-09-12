b8064fed6ba423e3a52163adb782d51d2ceedf94
Katarina Dries <katarinadries@gmail.com>
2022-09-12 08:07:59 -0500

    upd

--- src/Prog54b.java
+++ src/Prog54b.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Prog54b {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in)
+        Scanner scan = new Scanner(System.in);
 
     }
 }

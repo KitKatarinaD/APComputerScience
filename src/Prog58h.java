c20f2a6d74bb915438ea6838e44952384b5b76f0
Katarina Dries <katarinadries@gmail.com>
2022-09-15 22:29:40 -0500

    upd

--- src/Prog58h.java
+++ src/Prog58h.java
@@ -2,8 +2,15 @@
 //9/14/2022
 //Prog58h
 
+import java.util.Scanner;
+
 public class Prog58h {
     public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter your interest rate:" );
+        double r = scan.nextDouble();
+
 
 
 

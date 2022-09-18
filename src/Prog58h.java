a052a889f1c51d6c2e60db76621c861d3896fe5b
Katarina Dries <katarinadries@gmail.com>
2022-09-18 12:20:09 -0500

    upd

--- src/Prog58h.java
+++ src/Prog58h.java
@@ -8,8 +8,31 @@ public class Prog58h {
     public static void main(String [] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("Please enter the amount saved: ");
+        double P = scan.nextDouble();
+
         System.out.print("Please enter your interest rate:" );
         double r = scan.nextDouble();
+
+        System.out.print("Please enter the number of times your interest is compounded per year:");
+        double n = scan.nextDouble();
+
+        System.out.print("Please enter the number of days at interest: ");
+        double t = scan.nextDouble();
+
+        double A = P * (Math.pow((1 + ((.01 * r)/n)), ((n * t)/365) ) );
+
+        double S = A - P;
+
+        System.out.print("The interest earned is: " + S );
+
+        System.out.print("The total amount in savings is now: " + A);
+
+
+
+
+
+
 
 
 

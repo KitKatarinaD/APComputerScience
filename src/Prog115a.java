32b0ba8ab075fe5a3769cbfb602ed1f7ccf26614
dries.k <katarinadries@gmail.com>
2022-09-19 10:00:00 -0500

    Update

---
+++ src/Prog115a.java
@@ -0,0 +1,45 @@
+//Katarina Dries
+//9/19/22
+//Prog115a
+
+public class Prog115a {
+    public static void main(String [] args ) {
+        int lcv = 0; //lcv stands for loop control variable
+        while (lcv < 36) {
+            lcv += 2;
+            //     += stands for lcv = lcv + 2;
+            System.out.println(lcv);
+
+
+        }
+
+
+
+    }
+
+}
+
+/*
+C:\Users\dries.k\.jdks\openjdk-18.0.2.1\bin\java.exe "-javaagent:C:\Program Files (x86)\JetBrains\IntelliJ IDEA Community Edition 2021.3.1\lib\idea_rt.jar=61402:C:\Program Files (x86)\JetBrains\IntelliJ IDEA Community Edition 2021.3.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\dries.k\IdeaProjects\AP Computer Science\out\production\AP Computer Science" Prog115a
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+
+Process finished with exit code 0
+
+ */

ab47a63bdc9d3e2c39f215c68458fb92ff406cb2
Katarina Dries <katarinadries@gmail.com>
2022-09-18 21:46:27 -0500

    upd

--- src/Prog58t.java
+++ src/Prog58t.java
@@ -2,10 +2,32 @@
 //9/18/2022
 //Prog58t
 
+import java.util.Scanner;
+
 public class Prog58t {
+
+    public static void main(String[] args) {
+
+        Scanner Scan = new Scanner(System.in);
+
+        System.out.print("Please state the amount of the purchase: ");
+        double P = Scan.nextDouble();
+
+        System.out.print("Please state the amount received from the customer:");
+        double C = Scan.nextDouble();
+
+        double Output = C - P;
+
+        System.out.println("The cash back is: " + Output);
+
+    }
 }
-public static void main(String [] args) {
 
+/*
+C:\Users\16087\.jdks\openjdk-18.0.2.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.1\lib\idea_rt.jar=52146:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.1\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\16087\Desktop\APComputerScience\out\production\AP Computer Science" Prog58t
+Please state the amount of the purchase: 125.32
+Please state the amount received from the customer:140.00
+The cash back is: 14.680000000000007
 
-
-}
+Process finished with exit code 0
+ */

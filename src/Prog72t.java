c5a927995b80899bb3823e30b732a4c7433f0b2b
dries.k <katarinadries@gmail.com>
2022-09-27 10:30:47 -0500

    Update

---
+++ src/Prog72t.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Prog72t {
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the first time: ");
+        int time1 = input.nextInt();
+
+        System.out.println("Enter the second time: ");
+
+
+
+
+    }
+}

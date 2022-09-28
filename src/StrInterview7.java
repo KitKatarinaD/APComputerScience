ee7cb816b934edbf5e2746e6bca0ee6877e16474
dries.k <katarinadries@gmail.com>
2022-09-28 10:50:00 -0500

    Update

--- src/StrInterview7.java
+++ src/StrInterview7.java
@@ -12,24 +12,27 @@ public class StrInterview7 {
         System.out.print("Enter a new string: ");
         String mytext = keyboard.nextLine();
 
-        mytext = mytext.toLowerCase(Locale.ROOT);
+        mytext = mytext.toLowerCase(Locale.ROOT).trim();
 
 
         int vowelcount = 0;
+        int conscount = 0;
         for (int lcv = 0; lcv < mytext.length(); lcv++) {
             char cur = mytext.charAt(lcv);
             if (cur == 'a' || cur == 'e' || cur == 'i' || cur =='o' || cur =='u')
                 vowelcount++;
+            else if (cur != ' ') conscount++;
+
         }
 
-        int conscount = mytext.length() - vowelcount;
+
         System.out.printf("# Vowels: %d\t\t# Consonants: %d\n", vowelcount, conscount);
     }
 }
 
 /*
-C:\Users\dries.k\.jdks\openjdk-18.0.2.1\bin\java.exe "-javaagent:C:\Program Files (x86)\JetBrains\IntelliJ IDEA Community Edition 2021.3.1\lib\idea_rt.jar=51070:C:\Program Files (x86)\JetBrains\IntelliJ IDEA Community Edition 2021.3.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\dries.k\IdeaProjects\AP Computer Science\out\production\AP Computer Science" StrInterview7
-Enter a new string: Hello
+C:\Users\dries.k\.jdks\openjdk-18.0.2.1\bin\java.exe "-javaagent:C:\Program Files (x86)\JetBrains\IntelliJ IDEA Community Edition 2021.3.1\lib\idea_rt.jar=51251:C:\Program Files (x86)\JetBrains\IntelliJ IDEA Community Edition 2021.3.1\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\dries.k\IdeaProjects\AP Computer Science\out\production\AP Computer Science" StrInterview7
+Enter a new string: hello
 # Vowels: 2		# Consonants: 3
 
 Process finished with exit code 0

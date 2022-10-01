1d2f874e27b38044980181eed1360264c9a0b5da
Katarina Dries <katarinadries@gmail.com>
2022-10-01 01:32:05 -0500

    upd

--- src/Prog93a.java
+++ src/Prog93a.java
@@ -18,25 +18,54 @@ public class Prog93a {
         System.out.println(" ");
 
         double BR = 0.0475;
-        double BaseCost = Math.round(((KWH * BR) * 100.00) / 100.00);
+        double BaseCost = KWH * BR;
+
+        System.out.printf("Base Rate:  KWH @ $ %.2f\n", BR);
+        System.out.printf("     $ %.2f\n", BaseCost);
+        System.out.println(" ");
+        double SC = BaseCost * 0.10;
+        System.out.printf("Surcharge:        $ %.2f\n", SC);
+        System.out.println(" ");
+
+        double CT = BaseCost  * 0.03;
+        System.out.printf("City Tax:         $ %.2f\n", CT);
+        System.out.println(" ");
 
 
-        System.out.println("Base Rate:  KWH @ $ " + BR + "     $ " + BaseCost);
-        double SC = Math.round(((BaseCost * 0.10) * 100.00) / 100.00);
-        System.out.println("Surcharge:        $ " + SC);
-        double CT = Math.round(((BaseCost  * 0.03) * 100.00) / 100.00);
-        System.out.println("City Tax:         $ " + CT);
-
-        double TotCost = Math.round(((SC + CT + BaseCost) * 100.00) / 100.00);
+        double TotCost = SC + CT + BaseCost;
         System.out.println("               _______");
         System.out.println(" ");
-        System.out.println("Pay this amount:   $ " + TotCost);
+        System.out.printf("Pay this amount:   $ %.2f\n", TotCost);
         System.out.println(" ");
 
-        double FinedBill = Math.round(((TotCost * 1.04) * 100.00) / 100.00);
-        System.out.println("After May 20th Pay:   $ " + FinedBill);
+        double FinedBill = TotCost * 1.04;
+        System.out.printf("After May 20th Pay:   $ %.2f\n", + FinedBill);
 
 
 
     }
 }
+/*C:\Users\16087\.jdks\openjdk-18.0.2.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.1\lib\idea_rt.jar=52731:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.1\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\16087\Desktop\APComputerScience\out\production\AP Computer Science" Prog93a
+Enter KWH used:
+993
+     C O M P S C I Electric
+_________________________________
+KWH Used:  993.0
+_________________________________
+
+Base Rate:  KWH @ $ 0.05
+     $ 47.17
+
+Surcharge:        $ 4.72
+
+City Tax:         $ 1.42
+
+               _______
+
+Pay this amount:   $ 53.30
+
+After May 20th Pay:   $ 55.43
+
+Process finished with exit code 0
+
+ */

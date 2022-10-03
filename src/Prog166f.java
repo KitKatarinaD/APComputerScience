325e1a9f93fba643c04ecba55ed63bae3340c98a
Katarina Dries <katarinadries@gmail.com>
2022-10-02 19:14:51 -0500

    upd

---
+++ src/Prog166f.java
@@ -0,0 +1,34 @@
+public class Prog166f {
+    public static void main(String [] args) {
+        int num1 = 0;
+        int num2 = 0;
+
+        while (num1 < 110) {
+            num1 ++;
+            num2 = num1 + num2;
+            if (num1 == 100 || num1==101 ||num1 == 102 || num1==103 ||num1==104 ||num1==105 ||num1==106 || num1 == 107 || num1==108 ||num1 == 109 || num1==110 ){
+             System.out.println("The sum of the numbers from 1 to " + num1 + " is " + num2);
+            }
+        }
+
+
+
+    }
+}
+/*
+C:\Users\16087\.jdks\openjdk-18.0.2.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.1\lib\idea_rt.jar=52415:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.1\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\16087\Desktop\APComputerScience\out\production\AP Computer Science" Prog166f
+The sum of the numbers from 1 to 100 is 5050
+The sum of the numbers from 1 to 101 is 5151
+The sum of the numbers from 1 to 102 is 5253
+The sum of the numbers from 1 to 103 is 5356
+The sum of the numbers from 1 to 104 is 5460
+The sum of the numbers from 1 to 105 is 5565
+The sum of the numbers from 1 to 106 is 5671
+The sum of the numbers from 1 to 107 is 5778
+The sum of the numbers from 1 to 108 is 5886
+The sum of the numbers from 1 to 109 is 5995
+The sum of the numbers from 1 to 110 is 6105
+
+Process finished with exit code 0
+
+ */

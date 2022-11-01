9ff51eb1ebec773c0637d00205450033d54258e2
Katarina Dries <katarinadries@gmail.com>
2022-10-31 19:02:35 -0500

    upd

---
+++ src/cl76ahelper.java
@@ -0,0 +1,25 @@
+public class cl76ahelper {
+    private int Mynum;
+    private int mult;
+    private int Myans;
+    private int Mystep2;
+
+
+    public cl76ahelper (int n) {
+        Mynum = n;
+        Mystep2 = 0;
+        mult = 12345679;
+        Myans = 0;
+    }
+    public void calc() {
+        Mystep2 = Mynum * 9;
+        Myans = Mystep2 * mult;
+    }
+    public int getMyans() {
+        return Myans;
+    }
+    public int getMystep2() {
+        return Mystep2;
+    }
+
+}

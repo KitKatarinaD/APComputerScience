53f49de639ed45eec30afd8ac04d9bd1db389420
dries.k <katarinadries@gmail.com>
2022-11-03 10:29:44 -0500

    Update

--- src/LP56.java
+++ src/LP56.java
@@ -3,6 +3,11 @@ public class LP56 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a positive integer: ");
+        //will need to find a math way to do it...cannot change string to int bc not on ap subset
+
+
+
+        }
 
     }
 }

0b14dc1069f462bb7111cba86c223523deaa3532
Katarina <katarinadries@gmail.com>
2022-11-06 14:47:06 -0600

    upd

--- src/LP56.java
+++ src/LP56.java
@@ -4,8 +4,24 @@ public class LP56 {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a positive integer: ");
         //will need to find a math way to do it...cannot change string to int bc not on ap subset
+        int yup = input.nextInt();
+        int sum = 0;
+        int lcv = 0;
+        int x = (int) Math.pow(10, lcv);
+        // yup % 100 = 92     92/10=9     9 + ( 92 %\10)
+        //while (lcv!=yup) { lcv
+        while (x < yup) {
+        lcv ++;
+        }
+        lcv--;
 
 
+        while (lcv > -1){
+            yup += Math.floor((double)(yup/x)) + ((double) yup % x);
+            lcv--;
+        }
+
+       System.out.println(yup);
 
         }
 

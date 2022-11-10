484cc98be1f55235fda83186a62a47d93aeb9bf4
dries.k <katarinadries@gmail.com>
2022-11-10 10:02:16 -0600

    Update

---
+++ src/cl435g.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class cl435g {
+    public class dfile {
+        public static void main(String [] args) {
+            try {
+                Scanner input = new Scanner(new File("data/prog285b.dat"));
+
+            } catch (IOException e) {
+                System.out.println("Can't find data file!");
+            }
+        }
+    }
+
+
+}

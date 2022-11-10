481d28ff89b2591bb9ea1bf46fea556d474e5607
dries.k <katarinadries@gmail.com>
2022-11-10 09:17:23 -0600

    Update

--- src/cl213f.java
+++ src/cl213f.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class cl213f {
     public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("data/prog213f.dat"));
+            while (input.hasNext()){
+                double kwh = input.nextDouble();
+                cl213fhelper help = new cl213fhelper(kwh);
+                help.calc();
+                if (kwh != -999) {
+                    System.out.println(help.toString());
+                }
+            }
+
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+
+
+
 
 
     }

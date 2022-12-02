6cc0961ccf8fc72da3e6de257110ef6772e26458
dries.k <katarinadries@gmail.com>
2022-12-02 10:30:22 -0600

    Update

--- src/Prog402aClass.java
+++ src/Prog402aClass.java
@@ -1,2 +1,20 @@
 public class Prog402aClass {
+    private int myid;
+    private int myscore;
+    private double mydiff;
+
+    public Prog402aClass(int id, int score) {
+        myid = id;
+        myscore = score;
+        mydiff = 0;
+    }
+
+    public void calc(double avg) {
+        mydiff = myscore - avg;
+    }
+
+
+
+
+
 }
--- src/prog402a.java
+++ src/prog402a.java
@@ -1,2 +1,39 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class prog402a {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("data/prog402a.dat"));
+            Prog402aClass[] list = new Prog402aClass[21];
+            int size= 0;
+            while (input.hasNext()) {
+                int id = input.nextInt();
+                int score = input.nextInt();
+                Prog402aClass vector = new Prog402aClass(id, score);
+                list[size] = vector;
+                size++;
+            }
+
+            for (int index = 0; index < size; index++) {
+                list[index].calc(avg);
+
+
+
+            }
+
+
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
 }
+
+
+
+        //read in the data file
+        //calculate the avg
+        //call calc by passing in the avg---current.calc(avg)

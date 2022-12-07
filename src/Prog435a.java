c6b2e4cd3f17d4df056d432ef174dd8abf6c45a0
dries.k <katarinadries@gmail.com>
2022-12-07 10:48:53 -0600

    Update

--- src/Prog435a.java
+++ src/Prog435a.java
@@ -1,2 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Prog435a {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("data/prog435a.dat"));
+            Prog435aClass[] lot = new Prog435aClass[50];
+            int size = 0;
+            while (input.hasNext()) {
+                double factor = input.nextDouble();
+                int gate = input.nextInt();
+
+                Prog435aClass car = new Prog435aClass(factor, gate);
+
+                lot[size] = car;
+                size++;
+            }
+
+            System.out.println("Car Type:\t\tBase Toll\t\tFactor\t\tCost\t\t");
+
+            for (int index=0; index < size; index++) {
+                lot[index].calc();
+                Prog435aClass cur = lot[index];
+                System.out.print(cur.getMytype() + "\t\t");
+                System.out.printf("$%.2f\t\t%.2f\t\t$%.2f\n", cur.getMytoll(), cur.getMyfactor(), cur.getMycost());
+
+            }
+
+
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
 }
--- src/Prog435aClass.java
+++ src/Prog435aClass.java
@@ -22,8 +22,23 @@ public class Prog435aClass {
         else if (myfactor == 2.4) {mytype = "Truck";}
         else if (myfactor == 2.7) {mytype = "16 Wheeler";}
 
+        if (mygate == 1) { mytoll=1.35;}
+        else if (mygate == 2) { mytoll=2.00;}
+        else if (mygate == 3) { mytoll=2.50;}
+        else if (mygate == 4) { mytoll=3.25;}
+        else if (mygate == 5) { mytoll=4.10;}
+        else if (mygate == 6) { mytoll=4.8;}
+        else if (mygate == 7) { mytoll=5.50;}
+        else if (mygate == 8) { mytoll=6.00;}
 
+        mycost = mytoll * myfactor;
     }
+
+    public double getMyfactor() {return myfactor;}
+    public String getMytype() {return mytype;}
+    public int getMygate() {return mygate;}
+    public double getMytoll() {return mytoll;}
+    public double getMycost() {return mycost;}
 
 //reads in several pairs of data (vehicle type, Gate number) and outputs the
 //Name of the vehicle type, the toll, the factor, and total bill for turnpike use.

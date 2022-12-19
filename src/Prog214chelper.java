ad3253bfb4444143bffe5bee0c84604e03215645
dries.k <katarinadries@gmail.com>
2022-12-19 10:32:57 -0600

    Update

--- src/Prog214chelper.java
+++ src/Prog214chelper.java
@@ -14,10 +14,26 @@ public class Prog214chelper {
     }
 
     public void calc() {
-     // if (myR > || myP || myH)
+      if (myR > 10 || myP || myH)
         mytot = myR + myP + myH + mywash;
 
     }
+    /*
+    A service station with a car wash sells three grades of gasoline:
+     regular unleaded at $1.359 per gallon,
+     premium at $1.479 per gallon, and high octane at $1.429 per gallon
+      A car wash normally costs $2.00, but the station offers a
+      10% reduction on the price of a wash for every full gallon of
+      gasoline purchased in the excess of the first 10 gallons.
+       When 20 or more gallons are purchased, the car wash is free.
+       Write a program that reads the number of gallons purchased by a
+       customer and a character (R, P, or H) indicating the grade of
+        gasoline purchased.  The program should print the cost of the
+         gasoline and the price of a wash, read another character
+          indicates whether or not the customer wants a car wash,
+          and then print the total charges in the form of an invoice
+
+     */
 
 
 

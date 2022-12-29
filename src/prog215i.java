ee8f8d316cd58f11e00eef89d2f6f1eede64ca7a
Katarina <katarinadries@gmail.com>
2022-12-29 17:47:08 -0600

    upd

--- src/prog215i.java
+++ src/prog215i.java
@@ -7,7 +7,6 @@ public class prog215i {
         try {
             Scanner input = new Scanner(new File("data/prog215i.dat "));
             int numpumps = input.nextInt();
-            input.nextLine();
             ArrayList<prog215iclass> pump = new ArrayList<prog215iclass>();
 
 
@@ -15,14 +14,38 @@ public class prog215i {
                 int vehicle = input.nextInt();
                 double gal = input.nextDouble();
                 double miles = input.nextDouble();
-                input.nextLine();
                 pump.add(new prog215iclass(vehicle, gal, miles));
             }
+            double min = pump.get(0).getMympg();
+            double max = pump.get(0).getMympg();
+            System.out.println("MPG Statistics: \nWorst:");
+            for (int i = 1; i < numpumps; i++) {
+                if (pump.get(i).getMympg() < min) {
+                    min = pump.get(i).getMygal();
+                }
+            }
+            double avg = 0;
+            System.out.println("\t\t\tBest:");
+            for (int i = 1; i < numpumps; i++) {
+                if (pump.get(i).getMympg() > max) {
+                    max = pump.get(i).getMympg();
+                }
+            }
+            for (int lcv = 1; lcv < numpumps; lcv++) {
+                avg += pump.get(lcv).getMympg();
+            }
+            avg = avg/numpumps;
+
+            System.out.println("Fleet Avg:\t\t" + avg );
+
+
+            System.out.println("Number of vehicles: \t" + numpumps);
 
 
             System.out.println("Vehicle\tMiles\tGallons\tMPG");
             for (prog215iclass car : pump) {
                 System.out.println(pump.get(car.getMyvehicle()) + "\t" + car.getMymiles() + "\t" + car.getMygal() + "\t" + car.getMympg());
+
             }
 
 

9e2583913b2e54f4beec80999300d1b453dbbccc
Katarina <katarinadries@gmail.com>
2022-12-30 17:31:02 -0600

    upd

--- src/prog215iclass.java
+++ src/prog215iclass.java
@@ -16,14 +16,8 @@ public class prog215iclass {  //one vehicle
         mympg = mymiles/mygal;
     }
 
-    public void setMygal(double gal) { mygal = gal;}
-    public void setMyvehicle(int vehicle)  {myvehicle = vehicle;}
-    public void setMymiles(double miles) {mymiles = miles;}
-
     public int getMyvehicle() {return myvehicle;}
     public double getMygal() {return mygal;}
     public double getMymiles() {return mymiles;}
     public double getMympg() {return mympg;}
-
-
 }

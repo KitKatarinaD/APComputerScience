d8374d8b27149ddaf0901c01a6a46ca855d4281a
Katarina <katarinadries@gmail.com>
2023-03-05 19:59:24 -0600

    upd

---
+++ src/Q3/prog910b.java
@@ -0,0 +1,19 @@
+package Q3;
+
+import java.util.Scanner;
+
+public class prog910b {
+    public static String conversion(int n) {
+        if (n==0) return "0";
+        if (n==)
+
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number base 10 to convert: ");
+        int num = input.nextInt();
+
+    }
+
+}

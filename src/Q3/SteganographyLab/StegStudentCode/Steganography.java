38abecb3e1ba6442ab24a0f3f12d39e09b489ccb
Katarina <katarinadries@gmail.com>
2023-04-25 09:25:23 -0500

    upd

--- src/Q3/SteganographyLab/StegStudentCode/Steganography.java
+++ src/Q3/SteganographyLab/StegStudentCode/Steganography.java
@@ -1,5 +1,14 @@
 package Q3.SteganographyLab.StegStudentCode;
+import java.awt.Color;
 
+/**
+ * Clear the lower (rightmost) two bits in a pixel.
+ */
+public static void clearLow(Pixel p){
+
+        }
 public class Steganography {
+    public static void main(String[] args) {
 
+    }
 }
